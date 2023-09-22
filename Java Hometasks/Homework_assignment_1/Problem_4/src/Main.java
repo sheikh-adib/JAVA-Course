2d2e7b258a668ccbbe9093f8e92a8ecaffac1277
sheikh-adib <sheikh.binshabubakar001@stud.fh-dortmund.de>
2023-09-22 12:47:25 +0200

    Update Main.java

--- Java Hometasks/Homework_assignment_1/Problem_4/src/Main.java
+++ Java Hometasks/Homework_assignment_1/Problem_4/src/Main.java
@@ -7,10 +7,14 @@
  * Subtask: Problem 3
  */
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+
+import static java.lang.Character.isLetter;
 
 public class Main {
     public static void main(String[] args) {
-
         // text
         String text = "To be or not to be, that is the question;"
                 +"Whether `tis nobler in the mind to suffer"
@@ -18,31 +22,71 @@ public class Main {
                 +" or to take arms against a sea of troubles,"
                 +" and by opposing end them?";
 
-        // define word - continues sequence of letters (start with a letter or ` and end with  space or ')
-        // store all word in a list
+        // 1. define word - continues sequence of letters (start with a letter or ` and end with  space or ')
+        // 2. collect all word in a list
+        // 3. remove redundant word (optional)
+        // 4. sort out the list
 
+        String word = "";
+        ArrayList<String> wordList= new ArrayList<String>();
+        boolean removeRedundant = true;
+
+        // 2. collect all word in a list
         for (int i = 0; i < text.length(); i++)
         {
             char c = text.charAt(i);
 
-            //start of word
-            if( c == '`' )
+            //start of words
+            if( c == '`' || isLetter(c))
             {
-                continue;
+                //end of words
+                for(char cInWord; text.charAt(i) != ' ' && text.charAt(i) != ';' && text.charAt(i) != '?' && text.charAt(i) != ','; i++)
+                {
+                    cInWord = text.charAt(i);
+                    word += cInWord;
+                }
+                // store words
+                //System.out.println(word);
+                wordList.add(word);
+
+                //clear words
+                word = "";
+                i--;
             }
 
         }
 
-        System.out.println("The text contained: \n" + vowels + " vowels, \n" + (letters - vowels) + " consonants, \n" + spaces + " spaces, \n" + letters + " letters.");
+        // 3. remove redundant word
+        if(removeRedundant)
+        {
+            for(int i =0; i< wordList.size();i++)
+            {
+                String currentWord = wordList.get(i).toLowerCase();
+                for(int j = i + 1; j<wordList.size();j++)
+                {
+                    if(Objects.equals(currentWord, wordList.get(j).toLowerCase()))
+                    {
+                        wordList.remove(j);
+                    }
+                }
+            }
+        }
 
-        // sort the word
+        // sort out the list based on first character
+        for(int i =0; i< wordList.size();i++)
+        {
+            String currentWord = wordList.get(i).toLowerCase();
+            for(int j = i + 1; j<wordList.size();j++)
+            {
+                if((int)currentWord.charAt(0) > (int)wordList.get(j).toLowerCase().charAt(0))
+                {
+                    Collections.swap(wordList, i, j);
+                    currentWord = wordList.get(i).toLowerCase();
+                }
+            }
+        }
 
+        System.out.println(wordList);
     }
 }
 
-// number of letter (skip if the character is not a letter)
-            if(c == ' ' || c == '`' || c == ';' || c == '?' || c == ',')
-                    {
-                    continue;
-                    }
-                    letters++;
